b1ccadc7558d98ffd0ba3363e7f1a9d219ceb245
Maxime Ollivier <maxime-ollivier@hotmail;fr>
2023-05-04 11:56:25 +0200

    Increment 3

--- tracking-orders-assignment/src/pages/order.tsx
+++ tracking-orders-assignment/src/pages/order.tsx
@@ -1,3 +1,5 @@
+import styles from "@/styles/Order.module.css";
+
 import { useGetOrderByNumberQuery } from "../pages/api/orders";
 
 export default function Order() {
@@ -7,5 +9,32 @@ export default function Order() {
     error,
   } = useGetOrderByNumberQuery("UK1876YH08_2");
 
-  return <>{JSON.stringify(order)}</>;
+  console.log("Data", order);
+
+  return (
+    <>
+      <h1>Order info</h1>
+
+      <div className={styles.container}>
+        <ul className={styles.list_container}>
+          <li>
+            <span>Order ID:</span>
+            <span>{order?.id.toString()}</span>
+          </li>
+          <li>
+            <span>First Name:</span>
+            <span>{order?.first_name}</span>
+          </li>
+          <li>
+            <span>Last Name:</span>
+            <span>{order?.last_name}</span>
+          </li>
+          {/* <li>
+            <span>Trackers:</span>
+            <span>{order?.}</span>
+          </li> */}
+        </ul>
+      </div>
+    </>
+  );
 }
